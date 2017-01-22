04310010b6e76a63b3c28f3de12e1950881a9f69
adria <adria@perezrovira.net>
2017-01-22 06:14:33 +0100

    added some analysis for deaths

--- analysis.py
+++ analysis.py
@@ -1,9 +1,11 @@
-import csv # The digger
-import numpy as np # The magic
+import csv
+import numpy as np
 from collections import Counter
+import datetime
 
-fileDataName  = open('data/allNacimientos.csv', "rb")
-reader = csv.reader(fileDataName)
+file  = open('data/allDefunciones.csv', "rb")
+#fileDataName  = open('data/allNacimientos.csv', "rb")
+reader = csv.reader(file)
 
 rownum = 0
 allData = []
@@ -19,7 +21,7 @@ for iRow, row in enumerate(reader):
         allData.append( row )
         ALLDATA.append( [x.upper() for x in row] )
 
-fileDataName.close()
+file.close()
 
 # put all data in  numpy
 npData = np.array(allData)
@@ -39,6 +41,38 @@ for idx in range(len(header)):
 
     if len(vec2) > 0:
         count = Counter(vec2)
-        print '  %-15s: %4d - %6d - (%5d | %s)' % (header[idx], vec2.size, len(count), count.most_common(1)[0][1], count.most_common(1)[0][0])
+        print '  %-15s: %4d - %6d - ( %4d | %s )' % (header[idx], vec2.size, len(count), count.most_common(1)[0][1], count.most_common(1)[0][0])
     else:
         print '  %-15s: %4d' % (header[idx], vec2.size)
+
+
+# for defunciones
+if True:
+
+    vec  = NPDATA[:,18]
+    vec2 = vec [ NPDATA[:,18] != '' ]
+    count = Counter(vec2)
+
+    print '\nOficios mas comunes'
+    for idx in range(len(count)):
+        print '%4d : %s' % (count.most_common()[idx][1], count.most_common()[idx][0])
+
+
+if True:
+    diffBurial = []
+    # we compare the death date DEFUNCION2 (8) with burial date ENTERRAMIENTO2 (11)
+    for ii, defuncion in enumerate(NPDATA[:,8]):
+        if (defuncion != '') and (NPDATA[ii,11] != ''):
+            try:
+                deathDate  = datetime.datetime.strptime(defuncion,     '%d/%m/%Y').date()
+                burialDate = datetime.datetime.strptime(NPDATA[ii,11], '%d/%m/%Y').date()
+                diffBurial.append( (burialDate - deathDate).days )
+
+    
+            except: 
+                pass
+
+    delay = Counter(diffBurial)
+    print '\nTiempo de la muerte al entierro'
+    for idx in range(len(delay)):
+        print '%4d : %d dias' % (delay.most_common()[idx][1], delay.most_common()[idx][0])
